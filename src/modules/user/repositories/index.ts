895b99005a873fa7d4427da462d9eaaab208630d
Jonas Brito <jonasexplore@gmail.com>
2021-09-19 11:43:34 -0300

    chore(user): add user enum

--- src/modules/user/repositories/index.ts
+++ src/modules/user/repositories/index.ts
@@ -1,2 +1,6 @@
 export { IUserRepository } from './IUserRepository';
 export { MemoryUserRepository } from './memory/MemoryUserRepository';
+
+export enum USER {
+  USER_REPOSITORY = 'USER_REPOSITORY',
+}
