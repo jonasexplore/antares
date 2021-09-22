eb90cebeda046880c17e1da9267194881f230d75
Jonas Brito <jonasexplore@gmail.com>
2021-09-22 08:33:19 -0300

    chore: fix imports

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -5,15 +5,18 @@ import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
-import { interceptorError } from '@infra/http/interceptors';
-
 import '@shared/containers';
+
+import { interceptorError } from '@infra/http/interceptors';
 import { routes } from '@infra/http/routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  morgan('dev', { skip: (_req, _res) => process.env.NODE_ENV === 'test' }),
+);
 app.use(helmet());
 app.use('/api', routes);
 app.use(errors());
